6e8585bb0b5bdc2cfe6dcfd2fecc15cca7e7d657
Danilo Bezerra <86009742+danilo-bezerra@users.noreply.github.com>
2023-04-12 12:51:08 -0300

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,82 +1,82 @@
-import { decrypt } from "./decrypt.js"
-import { encrypt } from "./encrypt.js"
-import "./toggle-theme.js"
+import { decrypt } from "./decrypt.js";
+import { encrypt } from "./encrypt.js";
+import "./toggle-theme.js";
 
-const btnEncrypt = document.querySelector("#btn-encrypt")
-const btnDecrypt = document.querySelector("#btn-decrypt")
-const btnCopy = document.querySelector("#btn-copy")
+const btnEncrypt = document.querySelector("#btn-encrypt");
+const btnDecrypt = document.querySelector("#btn-decrypt");
+const btnCopy = document.querySelector("#btn-copy");
 
-const textInput = document.querySelector(".textarea")
+const textInput = document.querySelector(".textarea");
 
-const resultContainer = document.querySelector('.result')
-const noResultContainer = document.querySelector('.no-result')
+const resultContainer = document.querySelector(".result");
+const noResultContainer = document.querySelector(".no-result");
 
 function getResultText() {
-    const resultTag = document.querySelector(".result__text")
-    return resultTag.innerText
+  const resultTag = document.querySelector(".result__text");
+  return resultTag.value;
 }
 
 function hideResultContainer() {
-    resultContainer.classList.remove("active")
-} 
+  resultContainer.classList.remove("active");
+}
 
 function showResultContainer() {
-    resultContainer.classList.add("active")
-}   
+  resultContainer.classList.add("active");
+}
 
 function hideNoResultContainer() {
-    noResultContainer.classList.add("inative")
-} 
+  noResultContainer.classList.add("inative");
+}
 
 function showNoResultContainer() {
-    noResultContainer.classList.remove("inative")
-} 
+  noResultContainer.classList.remove("inative");
+}
 
 function setResult(result) {
-    const resultTag = document.querySelector(".result__text")
-    resultTag.innerText = result
+  const resultTag = document.querySelector(".result__text");
+  resultTag.innerText = result;
 }
 
 function setTextInputValue(result) {
-    textInput.value = result
+  textInput.value = result;
 }
 
 function copyToClipboard(text) {
-    navigator.clipboard.writeText(text)
+  navigator.clipboard.writeText(text);
 }
 
 function copyTextToClipboard() {
-    copyToClipboard(getResultText())
+  copyToClipboard(getResultText());
 }
 
 function onEncrypt() {
-    if (textInput.value.trim()) {
-        const encrypted = encrypt(textInput.value)
-        hideNoResultContainer()
-        showResultContainer()
-        setResult(encrypted)
-        setTextInputValue("")       
-    } else {
-        showNoResultContainer()
-        hideResultContainer()
-    }
+  if (textInput.value.trim()) {
+    const encrypted = encrypt(textInput.value);
+    hideNoResultContainer();
+    showResultContainer();
+    setResult(encrypted);
+    setTextInputValue("");
+  } else {
+    showNoResultContainer();
+    hideResultContainer();
+  }
 }
 
 function onDecrypt() {
-    if (textInput.value.trim()) {
-        const decrypted = decrypt(textInput.value)
-        hideNoResultContainer()
-        showResultContainer()
-        setResult(decrypted)
-        setTextInputValue("")        
-    } else {
-        showNoResultContainer()
-        hideResultContainer()
-    }
+  if (textInput.value.trim()) {
+    const decrypted = decrypt(textInput.value);
+    hideNoResultContainer();
+    showResultContainer();
+    setResult(decrypted);
+    setTextInputValue("");
+  } else {
+    showNoResultContainer();
+    hideResultContainer();
+  }
 }
 
-btnEncrypt.addEventListener('click', onEncrypt)
+btnEncrypt.addEventListener("click", onEncrypt);
 
-btnDecrypt.addEventListener('click', onDecrypt)
+btnDecrypt.addEventListener("click", onDecrypt);
 
-btnCopy.addEventListener('click', copyTextToClipboard)
+btnCopy.addEventListener("click", copyTextToClipboard);
